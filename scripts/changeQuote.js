da4a8a1356b6fab34787ee93136752357340886f
ye-song <songyangen@gmail.com>
2021-05-23 18:41:34 +0800

    Update changeQuote.js

--- scripts/changeQuote.js
+++ scripts/changeQuote.js
@@ -26,15 +26,20 @@ var currentQuote = '',
   currentAuthor = '';
 
 function getQuotes() {
-  return (
-     function (data) {
-      if (typeof data === 'string') {
-        quotesData = JSON.parse(data);
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
         console.log('quotesData');
         console.log(quotesData);
       }
-  })
-
+    }
+  });
 }
 
 
