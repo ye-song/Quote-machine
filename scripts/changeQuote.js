676b922a11d2d17558952c6949c169fe726f7d90
ye-song <songyangen@gmail.com>
2021-05-24 13:38:56 +0800

    Update changeQuote.js

--- scripts/changeQuote.js
+++ scripts/changeQuote.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 import {quotes} from '../quotes.js';
 const projectName = 'random-quote-machine';
-var quotesData = quotes;
+let quotesData;
 
 /*
   Code by Gabriel Nunes
@@ -26,28 +26,26 @@ var colors = [
 var currentQuote = '',
   currentAuthor = '';
 
-/*
-function getQuotes() {
-  return $.ajax({
-    headers: {
-      Accept: 'application/json'
-    },
-    url:
-      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
-    success: function (jsonQuotes) {
-      if (typeof jsonQuotes === 'string') {
-        quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData');
-        console.log(quotesData);
+  function getQuotes() {
+    return $.ajax({
+      headers: {
+        Accept: 'application/json'
+      },
+      url:
+        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+      success: function (jsonQuotes) {
+        if (typeof jsonQuotes === 'string') {
+          quotesData = JSON.parse(jsonQuotes);
+          console.log('quotesData');
+          console.log(quotesData);
+        }
       }
-    }
-  });
-}
-*/
+    });
+  }
 
 function getRandomQuote() {
-  return quotesData[
-    Math.floor(Math.random() * quotesData.length)
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
   ];
 }
 
@@ -105,6 +103,9 @@ function getQuote() {
 }
 
 $(document).ready(function () {
-  getQuote();
+  getQuotes().then(() => {
+    getQuote();
+  });
+
   $('#new-quote').on('click', getQuote);
 });
